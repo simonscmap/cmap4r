d3fe147e449bb0f0880049757bfdcedf1e6068fb
Sangwon Hyun <robohyun66@gmail.com>
2019-09-18 23:47:10 -0700

    Add new functions mimicking pycmap functionality.

---
+++ package/cmap4r/R/colocalize-new.R
@@ -0,0 +1,90 @@
+
+
+##' Colocalizes the source variable (from source table) with a single target
+##' variable (from target table).  The tolerance parameters set the matching
+##' boundaries between the source and target data sets.  Returns a dataframe
+##' containing the source variable joined with the target variable.
+##' @param spname stored procedure name that executes the matching logic.
+##' @param sourceTable table name of the source data set.
+##' @param sourceVariable the source variable. The target variables are matched
+##'   (colocalized) with this variable.
+##' @param targetTables table names of the target data sets to be matched with
+##'   the source data.
+##' @param targetVariables variable names to be matched with the source
+##'   variable.
+##' @param dt1 start date or datetime.
+##' @param dt2 end date or datetime.
+##' @param lat1 start latitude [degree N].
+##' @param lat2 end latitude [degree N].
+##' @param lon1 start longitude [degree E].
+##' @param lon2 end longitude [degree E].
+##' @param depth1 start depth [m].
+##' @param depth2 end depth [m].
+##' @param timeTolerance float list of temporal tolerance values between pairs
+##'   of source and target datasets. The size and order of values in this list
+##'   should match those of targetTables. If only a single integer value is
+##'   given, that would be applied to all target datasets. This parameter is in
+##'   day units except when the target variable represents monthly climatology
+##'   data in which case it is in month units. Notice fractional values are not
+##'   supported in the current version.
+##' @param latTolerance float list of spatial tolerance values in meridional
+##'   direction [deg] between pairs of source and target data sets. If only one
+##'   value is given, that would be applied to all target data sets.
+##' @param lonTolerance float list of spatial tolerance values in zonal
+##'   direction [deg] between pairs of source and target data sets. If only one
+##'   value is given, that would be applied to all target data sets.
+##' @param depthTolerance float list of spatial tolerance values in vertical
+##'   direction [m] between pairs of source and target data sets. If only one
+##'   value is given, that would be applied to all target data sets.
+atomic_match <- function(spName, sourceTable, sourceVar, targetTable, targetVar,
+                         dt1, dt2, lat1, lat2, lon1, lon2, depth1, depth2,
+                         temporalTolerance, latTolerance, lonTolerance,
+                         depthTolerance){
+
+  ## Stringify all arguments and form Query
+  args = list(spName, sourceTable, sourceVar, targetTable, targetVar, dt1,
+              dt2, lat1, lat2, lon1, lon2, depth1, depth2, temporalTolerance,
+              latTolerance, lonTolerance, depthTolerance)
+
+  ## A helper function. islist is a boolean that says whether the
+  ## argument should be treated as a list
+  custom_toString <- function(x, islist){
+    if(length(x)>1 | islist){
+      x = sapply(x, function(b)paste0("'", b, "'"))
+      x = paste(x, collapse=",+")
+      x = paste0('[', x, ']')
+    } else {
+      x = toString(x)
+    }
+    return(x)
+  }
+
+
+  ## Define the ones that are list objects
+  type_args = rep(FALSE, 17)
+  ## type_args[c(3,4,14,15,16,17)] = TRUE ## Commented out for now, but most
+  ## likely needed!
+
+
+  ## Form EXEC statement (new);
+  args = Map(custom_toString, args, type_args)
+  myquery = sprintf("EXEC %s '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s'",
+                    args[1], args[2], args[3], args[4], args[5], args[6],
+                    args[7], args[8], args[9], args[10], args[11], args[12],
+                    args[13], args[14], args[15], args[16], args[17])
+
+  ## Issue query
+  return(query(myquery)) ## query is API().query in Python
+}
+
+##' Takes a custom query, issues a request to the API, and returns the results
+##' in form of a dataframe.
+##' @param myquery An "EXEC ..." string.
+##' @param apiKey The API Key.
+##' @param ... Rest of arguments to \code{query_url()}.
+query <- function(myquery){
+
+  ## Form query
+  payload = list(query = myquery)
+  request(payload, route="/api/data/query?")
+}
---
+++ package/cmap4r/R/new.R
@@ -0,0 +1,136 @@
+##' Helper to check error (very basic for now)
+check_error <- function(response){
+  return(response$status_code==200)
+  ## TODO: return the actual status corresponding to the status code, IF not
+  ## 200.
+}
+
+##' Helper to get response to tibble.
+process_response_to_tibble <- function(response){
+  a = content(response, "raw")
+  a = readr::read_csv(a)
+  return(a)
+}
+
+##' Validating the format of sp arguments.
+validate_sp_args <- function(table,
+                             variable, dt1, dt2, lat1, lat2, lon1, lon2,
+                             depth1, depth2){
+  ## NOT WRITTEN YET; TODO: mimic what the python does.
+  return(TRUE)
+}
+
+##' Identical function in python.
+cruises <- function(){
+  myquery = "EXEC uspCruises"
+  df = query(myquery)
+  return(df)
+}
+
+##' Identical function in python.
+get_catalog <- function(){
+  myquery = 'EXEC uspCatalog'
+  df = query(myquery)
+}
+
+
+
+##' Identical function in python.
+cruise_by_name <- function(cruisename){
+
+  ## Form EXEC statement.
+  myquery = sprintf("EXEC uspCruiseByName '%s' ", cruiseName)
+
+  ## Issue query
+  df = query(myquery)
+  return(df)
+  ## TODO: check the response for multiple cruises or invalid names.
+}
+
+##' Identical function in python.
+cruise_bounds <- function(cruisename){
+
+  df = cruise_by_name(cruisename)
+  myquery = sprintf("EXEC uspCruiseBounds %d", df[['ID']][1])
+  df = query(myquery)
+  return(df)
+}
+
+##' Identical function in python. (Returns a subset of data according to space-time constraints.)
+subset <- function(self, spName, table, variable, dt1, dt2, lat1, lat2, lon1,
+                   lon2, depth1, depth2){
+        query = sprintf('EXEC %s ?, ?, ?, ?, ?, ?, ?, ?, ?, ?', spName)
+        args = list(table, variable, dt1, dt2, lat1, lat2, lon1, lon2, depth1, depth2)
+        return(stored_proc(self, query, args))
+}
+
+##' Near-identical function in python. Executes a strored-procedure and returns
+##' the results in form of a dataframe. Similar to query()
+stored_proc <- function(self, query, args){
+        payload =list(tableName = args[1],
+                      fields = args[2],
+                      dt1 = args[3],
+                      dt2 = args[4],
+                      lat1 = args[5],
+                      lat2 = args[6],
+                      lon1 = args[7],
+                      lon2 = args[8],
+                      depth1 = args[9],
+                      depth2 = args[10],
+                      spName = strsplit(toString(query), " ")[[1]][2])
+
+        ## ## Special handling of dates (temporary)
+        ## payload$dt1 = paste0(payload$dt1, " 00:00:00")
+        ## payload$dt2 = paste0(payload$dt2, " 00:00:00")
+
+        assert_that(validate_sp_args(args[1], args[2], args[3], args[4], args[5],
+                                     args[6], args[7], args[8], args[9], args[10]))
+        request(payload, route= "/api/data/sp?")
+}
+
+##' Going from (payload -> response -> df). Near-identical function in Python.
+request <- function(payload, route, old=FALSE){
+
+  ## Hard coded
+  baseURL = 'https://simonscmap.com'
+  ## if(old){
+  ##   route = "/dataretrieval/sp?"
+  ## } else {
+  ##   route = "/api/data/sp?"
+  ## }
+  apiKey = "31095550-d3d9-11e9-9174-fdf4e45bb057"
+
+  ## Form query and send + retrieve
+  url_safe_query = urlencode_python(payload)
+  url = paste0(baseURL,
+               route,
+               url_safe_query)
+
+  response = httr::GET(url, add_headers(Authorization = paste0("Api-Key ", apiKey)))
+  stopifnot(check_error(response))
+  return(process_response_to_tibble(response))
+}
+
+
+
+##' Helper, UNTESTED!! Only works for match so far.
+urlencode_python <- function(payload){
+  ## assert_that(length(payload)==1)
+  all_items = Map(function(item, itemname){
+    payload = paste0(itemname, "=", item)
+    payload = URLencode(payload, reserved=TRUE)
+    payload = gsub("%3D", "=", payload)
+  }, payload, names(payload))
+  payload = paste(all_items, collapse="&")
+
+  ## One step too far; the spaces " " are converted to "+"
+  payload = gsub("%20", "+", payload)
+  return(payload)
+}
+
+
+##' Takes a cruise name and colocalizes the cruise track with the specified variable(s). Work in progress
+along_track <- function(self, cruise, targetTables, targetVars, depth1, depth2,
+                        temporalTolerance, latTolerance, lonTolerance, depthTolerance){
+
+}
